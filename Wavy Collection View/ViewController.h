77ada22eaec01dec303e821c723e0cffb47fe42c
Marc Maguire <maguire.marc@gmail.com>
2017-05-18 12:12:29 -0400

    Initial Commit

---
+++ Wavy Collection View/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Wavy Collection View
+//
+//  Created by Marc Maguire on 2017-05-18.
+//  Copyright © 2017 Marc Maguire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Wavy Collection View/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Wavy Collection View
+//
+//  Created by Marc Maguire on 2017-05-18.
+//  Copyright © 2017 Marc Maguire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
