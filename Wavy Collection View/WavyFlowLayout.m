1b6c00fd52c6805d2b9c0202c352b17c5da34009
Marc Maguire <maguire.marc@gmail.com>
2017-05-18 13:16:32 -0400

    custom layout displays cells evenly across the x axis with random values along the y axis

--- Wavy Collection View/WavyFlowLayout.m
+++ Wavy Collection View/WavyFlowLayout.m
@@ -22,4 +22,30 @@
     self.minimumInteritemSpacing = CGFLOAT_MAX;
 }
 
+-(NSArray<UICollectionViewLayoutAttributes *> *)layoutAttributesForElementsInRect:(CGRect)rect {
+    
+    NSArray<UICollectionViewLayoutAttributes *> *superAttrs = [super layoutAttributesForElementsInRect:rect];
+    
+    //need to copy attrs from super to avoid cached frame mismatch
+    
+    NSMutableArray<UICollectionViewLayoutAttributes *> *newAttrs = [[NSMutableArray alloc]init];
+    CGFloat xValue = self.collectionView.frame.size.width/[superAttrs count];
+    NSInteger count = 1;
+    for (UICollectionViewLayoutAttributes *attribute in superAttrs) {
+        UICollectionViewLayoutAttributes *newAttribute = [attribute copy];
+        
+        
+        CGFloat randY = arc4random_uniform(self.collectionView.frame.size.height);
+        
+        newAttribute.frame = CGRectMake(xValue * count, randY, self.itemSize.width, self.itemSize.height);
+        count ++;
+        [newAttrs addObject:newAttribute];
+    }
+    
+    
+    
+    return newAttrs;
+    
+}
+
 @end
